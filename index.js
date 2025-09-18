50ad47834420d295d92c392b4614f6f200ea318c
GiovanaaPaulino <144141649+GiovanaaPaulino@users.noreply.github.com>
2025-09-18 18:16:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
 console.log("Oi isso é um console.log");
+
+let a, b, c;
+a = 5;
+b = 6;
+c = 7;
+
+let pessoa = "Dimitri";
+let pessoas = "Dimitri";
+
+function minhafuncao() {
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+}
